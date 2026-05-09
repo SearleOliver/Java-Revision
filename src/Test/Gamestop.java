0b8decd87b98a0a46c13320afa762564ea9dd5e5
NOTuglyduckling <osearle65@gmail.com>
2026-05-10 00:13:49 +0200

    revision

--- src/Test/Gamestop.java
+++ src/Test/Gamestop.java
@@ -1,7 +1,9 @@
 package Test;
 
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Gamestop {
@@ -9,10 +11,36 @@ public class Gamestop {
 	private int capacity;
 	private Game[] games;
 	private int numOp;
+	private List<Student> loaners = new ArrayList<>();
+	private List<Game> shelf = new ArrayList<>();
 
 	public Gamestop(int capacity) {
 		this.capacity=capacity;
 		this.games = new Game[capacity];
+	}
+	
+	public void addLoaner(Student loaner) {
+		if (!loaners.contains(loaner)) {
+			loaners.add(loaner);
+		}
+	}
+	
+	public void shelfGame(Game game) {
+		shelf.add(0,game);
+	}
+	
+	public void reShelfGame(Game game) {
+		shelf.add(game);
+	}
+	
+	public Game remove(String title) {
+		Game removed = null;
+		Game copy =new Game(title,new Student(0,"g"),2000);
+		if (shelf.contains(copy)) {
+			int index = shelf.indexOf(copy);
+			removed = shelf.remove(index);
+		}
+		return removed;
 	}
 	
 	public boolean addGame(Game game) {
@@ -27,7 +55,7 @@ public class Gamestop {
 	
 	public boolean removeGame(Game game) {
 		boolean removed = false;
-		for (int i =0; i < numGames-1;i++) {
+		for (int i =0; i < numGames;i++) {
 			if (games[i].equals(game)) {
 				for (int j =i; j < numGames-1;j++) {
 					games[j]=games[j+1];
@@ -81,20 +109,30 @@ public class Gamestop {
 			numOp++;
 			numOpRef++;
 			numGames--;
+			indexIt--;
 			nextDone=false;
 		}
 		
-		public static void main(String args[]) {
-			Gamestop gs = new Gamestop(17);
-			Game gmod = new Game("gmod",new Student(21,"Oliver"),2008);
-			Game gta6 = new Game("GTAVI",new Student(73,"Bob"),2067);
-			gs.addGame(gmod); gs.addGame(gta6);
-			for (Iterator<Game> it = gs.iterator();it.hasNext();) {
-				
-			}
-			
+		
+	}
+	
+	public static void main(String[] args) {
+		Gamestop gs = new Gamestop(17);
+		Game gmod = new Game("gmod",new Student(21,"Oliver"),2008);
+		Game gta6 = new Game("GTAVI",new Student(73,"Bob"),2067);
+		Game gta5 = new Game("GTAV",new Student(45,"Greg"),2010);
+		Game wt = new Game("WarThunder",new Student(14,"Ivan"),2014);
+		gs.addGame(gmod); gs.addGame(gta6);
+		
+		gs.shelfGame(gmod);
+		gs.shelfGame(gta6);
+		gs.shelfGame(gta5);
+		gs.reShelfGame(wt);
+		for (Iterator<Game> it = gs.shelf.listIterator();it.hasNext();) {
+			Game current = it.next();
+			System.out.println(current.getName());
 		}
 		
 	}
-
+	
 }
